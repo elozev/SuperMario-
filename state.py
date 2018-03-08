542c6f82a8e924046742bea646d40484f0ec4eb2
Emil Lozev <emilfrogy@gmail.com>
2018-03-08 15:54:41 +0000

    State class created

---
+++ state.py
@@ -0,0 +1,10 @@
+class State:
+
+    def start(self):
+        # TODO: 1. Unload start screen 2. Load background with obstacles and mario
+        pass
+
+    def game_over(self):
+        # TODO: 1.Get results 2. Unload background with obstacles and mario
+        # TODO: 3. Display game over screen with presenting results 4. Button for restarting game
+        pass
